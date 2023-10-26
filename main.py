c60c5f20d93d402835f496a9568a1078a72f742e
Ned Petrov <nedd.petrov@gmail.com>
2023-10-26 15:02:58 +0300

    terminate driver upon exiting/needs fixing

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
+import sys
 from actions import Action
 from selenium.webdriver import Firefox, FirefoxOptions
 from conf import URL
 from time import sleep
 from pathlib import Path
 
+
+if not Path(Path.cwd().joinpath("ff_selenium_profile")).is_dir():
+    Path.cwd().joinpath("ff_selenium_profile").mkdir()
+
+options = FirefoxOptions()
+# options.add_argument('--headless')
+options.add_argument("--kiosk")
+options.add_argument('-profile')
+options.add_argument(Path.cwd().joinpath("ff_selenium_profile").as_posix())
+driver = Firefox(options)
+driver.set_window_size(1280, 720)
+action = Action(driver)
+
+driver.get(URL)
+
 try:
-    if not Path(Path.cwd().joinpath("ff_selenium_profile")).is_dir():
-        Path.cwd().joinpath("ff_selenium_profile").mkdir()
-
-    options = FirefoxOptions()
-    # options.add_argument('--headless')
-    options.add_argument("--kiosk")
-    options.add_argument('-profile')
-    options.add_argument(Path.cwd().joinpath("ff_selenium_profile").as_posix())
-    driver = Firefox(options)
-    driver.set_window_size(1280, 720)
-    driver.get(URL)
-    action = Action(driver)
-
     while True:
         if action.login():
             break
 
     while True:
         action.abawuwu()
+        action.tavern()
         action.arena()
         action.pets()
-        action.tavern()
+        action.fortress()
         sleep(200000)
 
-except:
+except KeyboardInterrupt:
+    print("Interrupt")
     driver.quit()
-    quit()
+except Exception as e:
+    print(e)
+    driver.quit()
+
