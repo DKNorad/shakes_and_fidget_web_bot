0885b47d73cd37508ebb17827297cbf32c651fc8
Ned Petrov <nedd.petrov@gmail.com>
2023-10-28 03:25:12 +0300

    fixed options in the gui

--- gui/options.py
+++ gui/options.py
@@ -17,57 +17,74 @@ class Options(ttk.Frame):
         col = ttk.Frame(self, padding=10)
         col.grid(row=0, column=0, sticky=NSEW)
 
-        self.underground_var = IntVar(col, 0)
-
         options = ttk.Labelframe(col, text='Options', padding=(15, 10))
         options.pack(side=TOP, fill=BOTH, expand=YES)
 
         # Tavern
-        tavern1 = ttk.Checkbutton(options, text='Tavern')
+        tavern1 = ttk.Checkbutton(options, text='Tavern', variable=self.controller.options.get("tavern"),
+                                  command=lambda: tavern_cbo.configure(
+                                      state=READONLY if self.controller.options.get("tavern").get() else DISABLED))
         tavern1.pack(fill=X, pady=5)
 
-        cbo = ttk.Combobox(master=options, values=['Average', 'Gold', 'Experience'], state=READONLY)
-        cbo.current(0)
-        cbo.pack(fill=X, padx=(20, 0), pady=5)
+        tavern_cbo = ttk.Combobox(master=options, values=['Average', 'Gold', 'Experience'], state=DISABLED,
+                                  textvariable=self.controller.options.get("tavern_type"))
+        tavern_cbo.current(0)
+        tavern_cbo.pack(fill=X, padx=(20, 0), pady=5)
 
         # Arena
-        arena1 = ttk.Checkbutton(options, text='Arena')
+        arena1 = ttk.Checkbutton(options, text='Arena', variable=self.controller.options.get("arena"),
+                                 command=lambda: arena_cbo.configure(
+                                     state=READONLY if self.controller.options.get("arena").get() else DISABLED))
         arena1.pack(fill=X, pady=5)
 
-        cbo = ttk.Combobox(master=options, values=['Average', 'Gold', 'Experience'], state=READONLY)
-        cbo.current(0)
-        cbo.pack(fill=X, padx=(20, 0), pady=5)
+        arena_cbo = ttk.Combobox(master=options, values=['Average', 'Gold', 'Experience'], state=DISABLED,
+                                 textvariable=self.controller.options.get("arena_type"))
+        arena_cbo.current(0)
+        arena_cbo.pack(fill=X, padx=(20, 0), pady=5)
 
         # Pets
-        pets1 = ttk.Checkbutton(options, text='Pets')
+        pets1 = ttk.Checkbutton(options, text='Pets', variable=self.controller.options.get("pets"))
         pets1.pack(fill=X, pady=5)
 
         # Fortress
-        fortress1 = ttk.Checkbutton(options, text='Fortress')
-        fortress1.pack(fill=X, pady=5)
+        fort1 = ttk.Checkbutton(options, text='Fortress', variable=self.controller.options.get("fortress"),
+                                command=lambda: [el.configure(state=NORMAL) for el in [fort2, fort3, fort4]]
+                                if self.controller.options.get("fortress").get()
+                                else [el.configure(state=DISABLED) for el in [fort2, fort3, fort4]])
+        fort1.pack(fill=X, pady=5)
 
-        fortress2 = ttk.Checkbutton(master=options, text='Collect experience from the Academy')
-        fortress2.pack(fill=X, padx=(20, 0), pady=5)
+        fort2 = ttk.Checkbutton(master=options, text='Collect experience from the Academy', state=DISABLED,
+                                variable=self.controller.options.get("fortress_exp"))
+        fort2.pack(fill=X, padx=(20, 0), pady=5)
 
-        fortress3 = ttk.Checkbutton(master=options, text='Collect stone from the Quarry')
-        fortress3.pack(fill=X, padx=(20, 0), pady=5)
+        fort3 = ttk.Checkbutton(master=options, text='Collect stone from the Quarry', state=DISABLED,
+                                variable=self.controller.options.get("fortress_stone"))
+        fort3.pack(fill=X, padx=(20, 0), pady=5)
 
-        fortress4 = ttk.Checkbutton(master=options, text='Collect wood from the Woodcutter\'s Hut')
-        fortress4.pack(fill=X, padx=(20, 0), pady=5)
+        fort4 = ttk.Checkbutton(master=options, text='Collect wood from the Woodcutter\'s Hut', state=DISABLED,
+                                variable=self.controller.options.get("fortress_wood"))
+        fort4.pack(fill=X, padx=(20, 0), pady=5)
 
         # Underground
-        underground1 = ttk.Checkbutton(options, text='Underground', variable=self.underground_var)
-        underground1.pack(fill=X, pady=5)
+        und1 = ttk.Checkbutton(options, text='Underground', variable=self.controller.options.get("underground"),
+                               command=lambda: [el.configure(state=NORMAL) for el in [und2, und3, und4]]
+                               if self.controller.options.get("underground").get()
+                               else [el.configure(state=DISABLED) for el in [und2, und3, und4]])
+        und1.pack(fill=X, pady=5)
 
-        underground2 = ttk.Checkbutton(master=options, text='Collect souls from the Soul Extractor')
-        underground2.pack(fill=X, padx=(20, 0), pady=5)
+        und2 = ttk.Checkbutton(master=options, text='Collect souls from the Soul Extractor', state=DISABLED,
+                               variable=self.controller.options.get("underground_souls"))
+        und2.pack(fill=X, padx=(20, 0), pady=5)
 
-        underground3 = ttk.Checkbutton(master=options, text='Collect gold from the Gold Pit')
-        underground3.pack(fill=X, padx=(20, 0), pady=5)
+        und3 = ttk.Checkbutton(master=options, text='Collect gold from the Gold Pit', state=DISABLED,
+                               variable=self.controller.options.get("underground_gold"))
+        und3.pack(fill=X, padx=(20, 0), pady=5)
 
-        underground4 = ttk.Checkbutton(master=options, text='Lure heroes underground')
-        underground4.pack(fill=X, padx=(20, 0), pady=5)
+        und4 = ttk.Checkbutton(master=options, text='Lure heroes underground', state=DISABLED,
+                               variable=self.controller.options.get("underground_lure"))
+        und4.pack(fill=X, padx=(20, 0), pady=5)
 
         # Dungeon
-        underground1 = ttk.Checkbutton(options, text='Dungeon', state=DISABLED)
-        underground1.pack(fill=X, pady=5)
+        dung1 = ttk.Checkbutton(options, text='Dungeon', state=DISABLED,
+                                variable=self.controller.options.get("dungeon"))
+        dung1.pack(fill=X, pady=5)
